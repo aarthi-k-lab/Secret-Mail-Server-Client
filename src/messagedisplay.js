e1bf27511e58b590c62db40526f02f34b1339a35
aKannan <Aarthi.kannan@genesys.com>
2021-05-16 10:18:44 +0530

    MessageDisplay

---
+++ src/messagedisplay.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class MessageDisplay extends Component {
+  state = {};
+  render() {
+    return <>MessageDisplay</>;
+  }
+}
+
+export default MessageDisplay;
